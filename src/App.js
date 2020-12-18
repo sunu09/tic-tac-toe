d5a6d589ffdfefa1dc046b8aa67c2c7547d60fb6
sunu09 <74618256+sunu09@users.noreply.github.com>
2020-12-17 21:48:43 -0600

    1st commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Game from './Components/Game';
+
+function App() {
+  return (
+    <Game />
+    
+  );
+}
+
+export default App;
